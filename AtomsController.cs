7424a9b47a0a63a7a87239ad867d2d20a8821fd9
neurospeech <ackava@gmail.com>
2014-04-28 00:47:06 +0530

    Draft

---
+++ AtomsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace NeuroSpeech.Atoms.Mvc
+{
+
+    /// <summary>
+    /// Encapsulates Secure Repository
+    /// </summary>
+    /// <typeparam name="TRepository"></typeparam>
+    public abstract class AtomsController<TRepository> : Controller
+        where TRepository:class,ISecureRepository
+    {
+
+        private TRepository _Repository = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TRepository Repository {
+            get {
+                return _Repository;
+            }
+        }
+
+        /// <summary>
+        /// Creates associated repository for this controller
+        /// </summary>
+        /// <returns></returns>
+        protected virtual TRepository CreateRepository()
+        {
+            return Activator.CreateInstance<TRepository>();
+        }
+
+        protected abstract BaseSecurityContext AttachSecurityContext(AuthenticationContext context);
+
+        /// <summary>
+        /// Creates Secure Repository
+        /// </summary>
+        /// <param name="requestContext"></param>
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+
+            _Repository = CreateRepository();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnAuthentication(System.Web.Mvc.Filters.AuthenticationContext filterContext)
+        {
+            base.OnAuthentication(filterContext);
+            BaseSecurityContext sc = AttachSecurityContext(filterContext);
+            _Repository.Initialize(User.Identity.Name, sc);
+        }
+
+        /// <summary>
+        /// Disposes the repository
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (_Repository != null) {
+                    _Repository.Dispose();
+                    _Repository = null;
+                }
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ISecureRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSpeech.Atoms.Mvc
+{
+    interface ISecureRepository  : IDisposable
+    {
+        IQueryable<T> Query<T>();
+
+        BaseSecurityContext SecurityContext { get; }
+
+        int Save();
+
+        void Initialize(string userName, BaseSecurityContext sc);
+
+        IDisposable CreateSecurityScope(BaseSecurityContext sc);
+
+    }
+
+
+}
